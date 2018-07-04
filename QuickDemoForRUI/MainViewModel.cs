122343202b79fb5d069c5ef10d5c6cc8e79b4f81
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2018-07-04 13:22:55 +0800

    添加项目文件。

---
+++ QuickDemoForRUI/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using System;
+using System.Threading.Tasks;
+namespace QuickDemoForRUI
+{
+    public class MainViewModel:ReactiveUI.ReactiveObject
+    {
+        public ReactiveCommand MyCommand { get; protected set; }
+        public MainViewModel()
+        {
+            MyCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                Console.WriteLine("foo"); //this works
+                await Task.Run(() => {
+                    
+                });
+            });//cause exception right after return
+        }
+    }
+}
---
+++ QuickDemoForRUI/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace QuickDemoForRUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; set; }
+        public MainWindow()
+        {
+            ViewModel = new MainViewModel();
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+    }
+}
